b9f1a624a3d390df99d78cca57b5096c3915abbd
hari.gharan <harigharan@Rameshs-Mac-mini.local>
2013-11-28 01:11:54 +0530

    initial commit

---
+++ tempgit/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  tempgit
+//
+//  Created by Harigharan on 28/11/13.
+//  Copyright (c) 2013 Softsuave Technology. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ tempgit/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  tempgit
+//
+//  Created by Harigharan on 28/11/13.
+//  Copyright (c) 2013 Softsuave Technology. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
